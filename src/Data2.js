eb2dc8b77d39c113935edcc94c4e81d7de9cb94e
Gonzalo Martinez <gonza.eze@gmail.com>
2021-07-24 23:28:02 -0300

    Proyecto Final

---
+++ src/Data2.js
@@ -0,0 +1,88 @@
+const data = {
+  status: "success",
+  items: [
+    {
+      id: 1,
+      title: "Pantalón Oxford",
+      price: 2500,
+      image: "https://ae01.alicdn.com/kf/He91a176b8f624ab591052e1da5a27903z/Pantalones-holgados-de-pierna-ancha-para-mujer-ropa-cl-sica-de-alta-calidad-a-la-moda.jpg_Q90.jpg_.webp",
+      category: "MARCA",
+      img1: "https://ae01.alicdn.com/kf/He91a176b8f624ab591052e1da5a27903z/Pantalones-holgados-de-pierna-ancha-para-mujer-ropa-cl-sica-de-alta-calidad-a-la-moda.jpg_Q90.jpg_.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321430&w=1000",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      title: "Blazer",
+      price: 3500,
+      image: "https://i.pinimg.com/736x/62/37/c8/6237c8a5053a1268e5362110af7cc92e.jpg",
+      category: "MARCA",
+      img1: "https://i.pinimg.com/736x/62/37/c8/6237c8a5053a1268e5362110af7cc92e.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606319825&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 3,
+      title: "Vestido",
+      price: 5000,
+      image: "https://ae01.alicdn.com/kf/Hb135d1e2398c4aafa9e8b8918536805bm/Vestido-de-oficina-de-alta-calidad-para-mujer-Ropa-de-Trabajo-Formal-elegante-ajustado-l-piz.jpg_Q90.jpg_.webp",
+      category: "MARCA",
+      img1: "https://ae01.alicdn.com/kf/Hb135d1e2398c4aafa9e8b8918536805bm/Vestido-de-oficina-de-alta-calidad-para-mujer-Ropa-de-Trabajo-Formal-elegante-ajustado-l-piz.jpg_Q90.jpg_.webp",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325289&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 4,
+      title: "Blusa",
+      price: 1000,
+      image: "https://sc04.alicdn.com/kf/HTB1TqhjENSYBuNjSsphq6zGvVXar.jpg",
+      category: "MARCA",
+      img1: "https://sc04.alicdn.com/kf/HTB1TqhjENSYBuNjSsphq6zGvVXar.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325338&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 5,
+      title: "Conjunto Deportivo",
+      price: 7500,
+      image: "https://i.pinimg.com/originals/86/5e/ce/865ecee3833bd5f6947072ffb6bb7c39.jpg",
+      category: "MARCA",
+      img1: " https://i.pinimg.com/originals/86/5e/ce/865ecee3833bd5f6947072ffb6bb7c39.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606322330&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 6,
+      title: "Chomba",
+      price: 1200,
+      image: "https://www.dhresource.com/600x600/f2/albu/g7/M00/2F/51/rBVaSluzWlaAXYh9AADY9JKCd8w578.jpg",
+      category: "MARCA",
+      img1: "https://www.dhresource.com/600x600/f2/albu/g7/M00/2F/51/rBVaSluzWlaAXYh9AADY9JKCd8w578.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606321824&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 7,
+      title: "Cárdigan",
+      price: 2500,
+      image: "https://image.dhgate.com/0x0s/f2-albu-g15-M00-50-F5-rBVa3l_situACqfdAAIwlJhzwnU538.jpg/mujer-dise-adores-ropa-2021-mujeres-su-teres.jpg",
+      category: "MARCA",
+      img1: "https://image.dhgate.com/0x0s/f2-albu-g15-M00-50-F5-rBVa3l_situACqfdAAIwlJhzwnU538.jpg/mujer-dise-adores-ropa-2021-mujeres-su-teres.jpg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606320966&w=1000",
+      cantidad: 1
+    },
+    {
+      id: 8,
+      title: "Calza",
+      price: 1000,
+      image: "https://esencialfit.com/wp-content/uploads/2019/09/0-2f70a1-640x640.jpeg",
+      category: "MARCA",
+      img1: "https://esencialfit.com/wp-content/uploads/2019/09/0-2f70a1-640x640.jpeg",
+      img2: ".jpg?auto=format,compress&q=90&updated_at=1606325265&w=1000",
+      cantidad: 1
+    },
+    
+  ],
+};
+
+export default data;
---
+++ src/components/page/inicio/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Portada from "../../../images/banner.jpg";
+
+export default function Inicio() {
+    return (
+        <div className="inicio">   
+            
+            <img src={Portada} alt=""/>
+            <p>TEMPORADA OTOÑO-INVIERNO 2021</p>
+        </div>
+    )
+}
